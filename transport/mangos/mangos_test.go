d56a35b949f41afaac9e5b439ec20e9a12148c3f
Rene Kaufmann <kaufmann.r@gmail.com>
2016-06-10 17:16:15 +0200

    removed TestMangosTransportError test

--- transport/mangos/mangos_test.go
+++ transport/mangos/mangos_test.go
@@ -1,7 +1,6 @@
 package mangos
 
 import (
-	"io"
 	"strings"
 	"testing"
 
@@ -105,76 +104,6 @@ func TestMangosTransportCommunication(t *testing.T) {
 
 	if string(rm.Body) != string(m.Body) {
 		t.Errorf("Expected %v, got %v", m.Body, rm.Body)
-	}
-
-	close(done)
-}
-
-func TestMangosTransportError(t *testing.T) {
-	tr := NewTransport()
-
-	l, err := tr.Listen(":0")
-	if err != nil {
-		t.Errorf("Unexpected listen err: %v", err)
-	}
-	defer l.Close()
-
-	fn := func(sock transport.Socket) {
-		defer sock.Close()
-
-		for {
-			var m transport.Message
-			if err := sock.Recv(&m); err != nil {
-				if err == io.EOF {
-					return
-				}
-				t.Fatal(err)
-			}
-
-			//sock.(*transportSocket).error(&Message{
-			//	Body: []byte(`an error occurred`),
-			//})
-		}
-	}
-
-	done := make(chan bool)
-
-	go func() {
-		if err := l.Accept(fn); err != nil {
-			select {
-			case <-done:
-			default:
-				t.Errorf("Unexpected accept err: %v", err)
-			}
-		}
-	}()
-
-	c, err := tr.Dial(l.Addr())
-	if err != nil {
-		t.Errorf("Unexpected dial err: %v", err)
-	}
-	defer c.Close()
-
-	m := transport.Message{
-		Header: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Body: []byte(`{"message": "Hello World"}`),
-	}
-
-	if err := c.Send(&m); err != nil {
-		t.Errorf("Unexpected send err: %v", err)
-	}
-
-	var rm transport.Message
-
-	err = c.Recv(&rm)
-	if err == nil {
-		t.Fatal("Expected error but got nil")
-	}
-
-	if err.Error() != "500 Internal Server Error: an error occurred" {
-		t.Fatalf("Did not receive expected error, got: %v", err)
 	}
 
 	close(done)
